792310920470127f4d8d6d341cd0ff6f27d2695c
Colin Young <hello@colin-young.co.uk>
2015-01-09 18:34:00 +0000

    remove old function from startup class

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -2,16 +2,4 @@ class Startup < ActiveRecord::Base
 
   validates_presence_of :name, :year_founded, :url, :status
 
-
-  def self.create_from_typeform answers
-    data = {}
-    eval(ENV.fetch('TYPEFORM_FIELDS')).each do |k, v|
-      data[k] = answers[v]
-    end
-
-    startup = create(data)
-
-    startup
-  end
-
 end
