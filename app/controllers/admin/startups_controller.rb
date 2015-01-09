213e2f764307a696f5d6a8cbbe2a178f2eb3c6fd
Colin Young <hello@colin-young.co.uk>
2015-01-09 18:33:14 +0000

    validation on update startup

--- app/controllers/admin/startups_controller.rb
+++ app/controllers/admin/startups_controller.rb
@@ -1,12 +1,33 @@
 module Admin
   class StartupsController < BaseController
+    before_action :set_startup, only: [:edit, :update, :destroy]
 
     def index
       @startups = Startup.all
     end
 
     def edit
+    end
+
+    def update
+      if @startup.update(startup_params)
+        redirect_to admin_startups_path, notice: 'Startup was updated'
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+    end
+
+    private
+
+    def set_startup
       @startup = Startup.find(params[:id])
+    end
+
+    def startup_params
+      params.require(:startup).permit(:name, :year_founded, :location, :description, :url, :twitter, :uploaded_logo, :status)
     end
 
   end
--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,5 +1,8 @@
 class Startup < ActiveRecord::Base
 
+  validates_presence_of :name, :year_founded, :url, :status
+
+
   def self.create_from_typeform answers
     data = {}
     eval(ENV.fetch('TYPEFORM_FIELDS')).each do |k, v|
