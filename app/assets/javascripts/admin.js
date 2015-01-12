9edd9f19951165b7b44d28917efc2de31bde5e44
Colin Young <hello@colin-young.co.uk>
2015-01-12 17:06:02 +0000

    adding redactor

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,3 +1,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require alerts
+//= require redactor.min
+
+$(document).ready(function(){
+  $('textarea').redactor({
+    buttons: ['html', 'formatting', 'bold', 'italic']
+  });
+});
