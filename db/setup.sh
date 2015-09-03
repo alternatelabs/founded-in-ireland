11f74f549c813280fc7de77c4e0b89eb7b4d6d26
Pete Hawkins <pete@phawk.co.uk>
2015-09-03 11:22:33 +0100

    Updates for rails 4.2.3

--- db/setup.sh
+++
@@ -1,8 +0,0 @@
-psql -c "CREATE DATABASE founded_in_ireland_dev;"
-psql -c "CREATE USER founded_in_ireland_dev WITH PASSWORD 'test';"
-psql -c "ALTER DATABASE founded_in_ireland_dev OWNER to founded_in_ireland_dev;"
-psql -c "GRANT ALL PRIVILEGES ON DATABASE founded_in_ireland_dev to founded_in_ireland_dev;"
-psql -c "CREATE DATABASE founded_in_ireland_test;"
-psql -c "CREATE USER founded_in_ireland_test WITH PASSWORD 'test';"
-psql -c "ALTER DATABASE founded_in_ireland_test OWNER to founded_in_ireland_test;"
-psql -c "GRANT ALL PRIVILEGES ON DATABASE founded_in_ireland_test to founded_in_ireland_test;"
